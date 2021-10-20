d310334df7739a00ff7f71a555cec5cf3870dc88
zmgkgkt1004 <78059383+zmgkgkt1004@users.noreply.github.com>
2021-10-19 22:44:39 -0500

    html js
    add required

--- js/script.js
+++ js/script.js
@@ -1,86 +1,15 @@
+/*Confirm_password JS*/
+function validateForm() {
+    //collect form data in JavaScript variables
+    var pw = document.getElementById("password").value;
+    var cpw = document.getElementById("confirm_password").value;
+    
 
-  document.addEventListener("DOMContentLoaded", function() {
-
-    // JavaScript form validation
-
-    var checkPassword = function(str)
-    {
-      var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
-      return re.test(str);
-    };
-
-    var checkForm = function(e)
-    {
-      if(this.username.value == "") {
-        alert("Error: Username cannot be blank!");
-        this.username.focus();
-        e.preventDefault(); // equivalent to return false
-        return;
-      }
-      re = /^\w+$/;
-      if(!re.test(this.username.value)) {
-        alert("Error: Username must contain only letters, numbers and underscores!");
-        this.username.focus();
-        e.preventDefault();
-        return;
-      }
-      if(this.pwd1.value != "" && this.pwd1.value == this.pwd2.value) {
-        if(!checkPassword(this.pwd1.value)) {
-          alert("The password you have entered is not valid!");
-          this.pwd1.focus();
-          e.preventDefault();
-          return;
-        }
-      } else {
-        alert("Error: Please check that you've entered and confirmed your password!");
-        this.pwd1.focus();
-        e.preventDefault();
-        return;
-      }
-      alert("Both username and password are VALID!");
-    };
-
-    var myForm = document.getElementById("myForm");
-    myForm.addEventListener("submit", checkForm, true);
-
-    // HTML5 form validation
-
-    var supports_input_validity = function()
-    {
-      var i = document.createElement("input");
-      return "setCustomValidity" in i;
+    if(pw1 != pw2) {
+      document.getElementById("message2").innerHTML = "**Passwords are not same";
+      return false;
+    } else {
+      alert ("Your password created successfully");
+      document.write("JavaScript form has been submitted successfully");
     }
-
-    if(supports_input_validity()) {
-      var usernameInput = document.getElementById("userName");
-      usernameInput.setCustomValidity(usernameInput.title);
-
-      var pwd1Input = document.getElementById("password");
-      pwd1Input.setCustomValidity(pwd1Input.title);
-
-      var pwd2Input = document.getElementById("confirm_password");
-
-      // input key handlers
-
-      usernameInput.addEventListener("keyup", function(e) {
-        usernameInput.setCustomValidity(this.validity.patternMismatch ? usernameInput.title : "");
-      }, false);
-
-      pwd1Input.addEventListener("keyup", function(e) {
-        this.setCustomValidity(this.validity.patternMismatch ? pwd1Input.title : "");
-        if(this.checkValidity()) {
-          pwd2Input.pattern = RegExp.escape(this.value);
-          pwd2Input.setCustomValidity(pwd2Input.title);
-        } else {
-          pwd2Input.pattern = this.pattern;
-          pwd2Input.setCustomValidity("");
-        }
-      }, false);
-
-      pwd2Input.addEventListener("keyup", function(e) {
-        this.setCustomValidity(this.validity.patternMismatch ? pwd2Input.title : "");
-      }, false);
-
-    }
-
-  }, false);
+ }
