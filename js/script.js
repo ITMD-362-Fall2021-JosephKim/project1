be83ddebf0f6df85fe5ae9fc736af9c44ee48636
zmgkgkt1004 <78059383+zmgkgkt1004@users.noreply.github.com>
2021-10-19 20:26:17 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*Confirm_password JS*/
 $('#password, #confirm_password').on('keyup', function () {
-	if ($('#password').val() == $('#confirm_password').val()) {
-		$('#message').html('Matching').css('color', 'green');
-	} else 
-		$('#message').html('Not Matching').css('color', 'red');
+    if ($('#password').val() == $('#confirm_password').val()) {
+        $('#message').html('Matching').css('color', 'green');
+    } else 
+        $('#message').html('Not Matching').css('color', 'red');
 });
