db01e44579584b1152d260aab3440bbdf26d348f
zmgkgkt1004 <78059383+zmgkgkt1004@users.noreply.github.com>
2021-10-19 20:05:11 -0500

    Update script.js
    Add js code for confirm_password

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-/*js*/
+/*Confirm_password JS*/
+$('#password, #confirm_password').on('keyup', function () {
+	if ($('#password').val() == $('#confirm_password').val()) {
+		$('#message').html('Matching').css('color', 'green');
+	} else 
+		$('#message').html('Not Matching').css('color', 'red');
+});
